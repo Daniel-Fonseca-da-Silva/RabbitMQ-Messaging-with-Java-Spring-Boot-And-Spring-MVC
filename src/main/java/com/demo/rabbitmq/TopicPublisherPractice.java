55f4b68e25c305d87ebd7495141bb61d79110354
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-10-30 14:07:00 -0300

    Created exercise to TopicPublisher

---
+++ src/main/java/com/demo/rabbitmq/TopicPublisherPractice.java
@@ -0,0 +1,22 @@
+package com.demo.rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class TopicPublisherPractice {
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		String msg = "This is a message to Computer and Radio!";
+
+		channel.basicPublish("Topic-Exchange-Practice", "computer.radio.car", null, msg.getBytes());
+	}
+
+}
